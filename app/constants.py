a5efd6ad79d3917dd0aa3216b68263a2ceee1c84
bowkiebo <43330666+kikie5021@users.noreply.github.com>
2024-08-05 20:00:33 +0700

    Add files via upload

---
+++ app/constants.py
@@ -0,0 +1,13 @@
+NON_PATHOGENIC = 0
+PATHOGENIC = 1
+CATEGORY = {
+    NON_PATHOGENIC: 'Non-Pathogenic',
+    PATHOGENIC: 'Pathogenic',
+    }
+
+NEGATIVE = 0
+POSITIVE = 1
+TESTING_RESULT = {
+    NEGATIVE: 'negative',
+    POSITIVE: 'positive',
+    }
